aac19ed9862e6a4ab29edf8b1857e649d22f66f0
kumbharjyotics232444-lab <kumbharjyotics232444@gmail.com>
2025-10-05 22:20:11 +0530

    Update User.js

--- static/js/User.js
+++ static/js/User.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String },          // only for manual signup
-  googleId: { type: String }           // only for Google signup
+  password: { type: String }         
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
